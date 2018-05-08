ad5512b699db2698021fbe742fbe1c9b12364dc1
bass3l <b.shmali@transtek.com>
2018-05-09 00:42:33 +0300

    add packet re-assembling and save response to file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using PcapDotNet.Core;
 using PcapDotNet.Packets;
 using PcapDotNet.Packets.IpV4;
@@ -49,19 +51,57 @@ namespace SnapTCP
 
         }
 
+        private static List<TcpDatagram> payloads = new List<TcpDatagram>();
+
         private static void PacketHandler(Packet packet){
 
             IpV4Datagram ip = packet.Ethernet.IpV4;
             TcpDatagram tcp = ip.Tcp;
 
-            if(ip.Destination.ToString() == "192.168.43.1" || ip.Source.ToString() == "192.168.43.1"){
+            if(/*ip.Destination.ToString() == "192.168.43.1" ||*/ ip.Source.ToString() == "192.168.43.1"){
                 Console.WriteLine("#PACKET:");
+                Console.WriteLine("SYN ?:" + tcp.IsSynchronize);
+                Console.WriteLine("ACK ?:" + tcp.IsAcknowledgment);
+                Console.WriteLine("FIN ?:" + tcp.IsFin);
+                Console.WriteLine("PUSH ?:" + tcp.IsPush);
                 Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + $" Length: {packet.Length}");
                 Console.WriteLine($"{ip.Source}:{tcp.SourcePort} -> {ip.Destination}:{tcp.DestinationPort}");
                 Console.WriteLine($"Sequence Number: {tcp.SequenceNumber}");
                 Console.WriteLine($"Payload Length: {tcp.PayloadLength}");
-                Console.WriteLine($"Payload: {tcp.Decode(System.Text.Encoding.ASCII)}");
+                
+                var payload = tcp.Decode(System.Text.Encoding.UTF8);
+                
+                Console.WriteLine($"Payload: {payload}");
+
+                if(tcp.IsAcknowledgment && !tcp.IsSynchronize && !tcp.IsFin){
+                    payloads.Add(tcp);
+                }else if (tcp.IsFin){
+                    Console.WriteLine("#### Response:");
+                    string response = String.Join("", payloads.Select(_tcp => {
+                        var _payload = _tcp.Decode(System.Text.Encoding.UTF8);
+                        if(_payload.Length != 0 && _payload.Length > 18)
+                            _payload = _payload.Substring(18, _payload.Length - 18);
+
+                        return _payload;
+                    }));
+                    
+                    //striping Http Header
+                    var startOfHttpBody = response.IndexOf("\r\n\r\n");
+
+                    if(startOfHttpBody == -1){
+                        Console.WriteLine("No Http Response");
+                        return;
+                    }
+
+                    Console.WriteLine($"idx:{startOfHttpBody}, len:{response.Length}");
+                    response = response.Substring(startOfHttpBody, response.Length - startOfHttpBody);
+                    
+                    Console.WriteLine(response);
+                    File.WriteAllText("response.html", response);
+                }
             }
+
+            payloads = payloads.OrderBy(_tcp => _tcp.SequenceNumber).ToList();
         }
     }
 }
